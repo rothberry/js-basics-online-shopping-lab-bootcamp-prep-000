b7da61399f2fe71bf9f8a744436ae164fd59255a
Philip Roth <phil.roth077@gmail.com>
2019-05-31 01:20:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    var 
+    
+  }
 }
 
 function total() {
