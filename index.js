15418edbad8c6411cc55acffe24473ad09fe1a5f
Philip Roth <phil.roth077@gmail.com>
2019-05-31 01:28:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random()*100)
+  var price = Math.floor(Math.random()*99)
   cart.push(new Object({[item]: price}))
   console.log(item + " has been added to your cart.");
   return cart
